5f97262eee0e90873aebf22f1fc9842689d5a2a6
flipmotion <flipmotions@gmail.com>
2015-10-12 21:36:57 +0300

    trash item
    trash item

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-	var owl = $('[data-item="owl-main"]');
-	owl.owlCarousel({
-		loop:true,
-		margin:0,
-		nav:true,
-		dots:false,
-		items:1,
-		autoplay:false,
-		navText: [
-			"<i class='my-arrow-left'></i>",
-			"<i class='my-arrow-right'></i>"
-		],
-		dots: true,
-	});
+	var owlMain = function(number,target) {
+		var owl = $(target);
+		owl.owlCarousel({
+			loop:true,
+			margin:0,
+			nav:true,
+			dots:false,
+			items:number,
+			autoplay:false,
+			navText: [
+				"<i class='my-arrow-left'></i>",
+				"<i class='my-arrow-right'></i>"
+			],
+			dots: true,
+		});
+	}
+	owlMain(1,'[data-item="owl-main"]');
+	owlMain(5,'[data-item="owl-main-card"]');
 });
 
