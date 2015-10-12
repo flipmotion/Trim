13e40022a384f43bc0c0f951a584a88e122327af
flipmotion <flipmotions@gmail.com>
2015-10-12 18:55:06 +0300

    home
    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-	
+	var owl = $('[data-item="owl-main"]');
+	owl.owlCarousel({
+		loop:true,
+		margin:0,
+		nav:true,
+		dots:false,
+		items:1,
+		autoplay:false,
+		navText: [
+			"<i class='my-arrow-left'></i>",
+			"<i class='my-arrow-right'></i>"
+		],
+		dots: true,
+	});
 });
 
